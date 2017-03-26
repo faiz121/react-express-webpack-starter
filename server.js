2de15bd0cf83c94088882b7e8749b250c2a949f1
Faiz Mohammed <faizmohammed178@gmail.com>
2017-03-26 16:11:36 -0700

    added json loader and css loader in webpack.config

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-require('babel-register');
+const express = require('express');
 
-require('./server.babel');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use('/', express.static('public'));
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+
